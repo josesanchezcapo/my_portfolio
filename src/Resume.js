9d97d08f77b64e67902cbcbfe9efd7d93b00eb60
Jose Sanchez-Capo <josesanchezcapo@gmail.com>
2021-03-23 10:48:46 -0500

    Adding Resume.js

---
+++ src/Resume.js
@@ -0,0 +1,13 @@
+// src/Resume.js
+
+import React from "react"
+
+function Resume(props) {
+    return (
+        <div>
+            <h1>Resume</h1>
+        </div>
+    )
+}
+
+export default Resume;
